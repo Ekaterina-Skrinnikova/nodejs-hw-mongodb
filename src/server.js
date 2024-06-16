6e5d614552ac12456b50ae85727c7927e213411e
Kateryna <kati.skrinnikova@gmail.com>
2024-06-16 15:12:22 +0300

    add changes in object successfull

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
-import contactsRouter from './routers/contacts.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
 const PORT = Number(env('PORT', 3000));
 
@@ -24,7 +24,30 @@ export const setupServer = () => {
     });
   });
 
-  app.get(contactsRouter);
+  app.get('/contacts', async (req, res, next) => {
+    const contacts = await getAllContacts();
+    res.status(200).json({
+      status: '200',
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+      next(new Error('Contact not found'));
+      return;
+    }
+
+    res.status(200).json({
+      status: '200',
+      message: `Successfully found contact with id=${contactId}!`,
+      data: contact,
+    });
+  });
 
   app.use('*', (req, res) => {
     res.status(404).json({
--- src/utils/ctrlWrapper.js
+++ src/utils/ctrlWrapper.js
@@ -1,9 +1,1 @@
-export const ctrlWrapper = (controller) => {
-  return async (req, res, next) => {
-    try {
-      await controller(req, res, next);
-    } catch (error) {
-      next(error);
-    }
-  };
-};
+
