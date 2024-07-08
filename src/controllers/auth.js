7056c742940e5d79d30da150f5b1f94457169903
Kateryna <kati.skrinnikova@gmail.com>
2024-07-08 10:38:59 +0300

    hash password

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -3,9 +3,14 @@ import { registerUser } from '../services/auth.js';
 export const registerUserController = async (req, res) => {
   const user = await registerUser(req.body);
 
+  const data = {
+    name: user.name,
+    email: user.email,
+  };
+
   res.json({
     status: 201,
     message: 'Successfully registered a user!',
-    data: user,
+    data,
   });
 };
